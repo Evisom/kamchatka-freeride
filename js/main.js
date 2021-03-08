d6f501b0ba56c2f7b0b29c9d167cbe2e8b73d870
chesminsky <chesminsky@gmail.com>
2021-03-08 20:48:03 +0300

    movies slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-$("#burger").click( () => {
-    if (
-        ($("#mobilemenu").attr("class")).split(" ").length == 1
-    ) {
-        $("#mobilemenu").addClass("navbar-mobilemenu-active")
+$('#burger').click(() => {
+    if ($('#mobilemenu').attr('class').split(' ').length == 1) {
+        $('#mobilemenu').addClass('navbar-mobilemenu-active')
     } else {
-        $("#mobilemenu").removeClass("navbar-mobilemenu-active")
+        $('#mobilemenu').removeClass('navbar-mobilemenu-active')
     }
 })
 
+$('#slider-movies').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    prevArrow: $('.slider-heading-controls-prev'),
+    nextArrow: $('.slider-heading-controls-next'),
+    variableWidth: true
+})
